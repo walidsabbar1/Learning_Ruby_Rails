86c8e0e0ad877331eb8720cd6885051ba3f87b0f
walidsabbar1 <wsabbar18@gmail.com>
2026-03-18 13:56:50 +0000

    feat: Implement full CRUD operations for posts, including new/edit views, controller actions, and simplified resource routes.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,46 @@
 class PostsController < ApplicationController
     def index
-      @posts = BlogPost.all
+      @posts = BlogPost.all.order(created_at: :desc)
     end
     def show
         @post = BlogPost.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
+    end
+    def new
+        @post = BlogPost.new
+    end
+    def create
+        @post = BlogPost.new(post_params)
+        if @post.save
+            redirect_to "/posts/#{@post.id}"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @post = BlogPost.find(params[:id])
+    end
+
+    def update
+        @post = BlogPost.find(params[:id])
+        if @post.update(post_params)
+            redirect_to "/posts/#{@post.id}"
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @post = BlogPost.find(params[:id])
+        @post.destroy
+        redirect_to root_path
+    end
+
+    private
+    def post_params
+        params.require(:blog_post).permit(:title, :body)
     end
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/posts/:id", to: "posts#show"
-  post "/posts", to: "posts#create"
-  put "/posts/:id", to: "posts#update"
-  patch "/posts/:id", to: "posts#update"
-  delete "/posts/:id", to: "posts#destroy"
+  resources :posts
 
 
   # Defines the root path route ("/")
